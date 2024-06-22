b5a57bb664a8b013c09d3aa1304c04b1567dc823
codesofC <inelusjudelin01@gmail.com>
2024-06-22 20:34:31 -0300

    Add auth pages

---
+++ app/(auth)/_layout.tsx
@@ -0,0 +1,12 @@
+import { Stack } from "expo-router";
+
+
+export default function AuthLayout() {
+
+    return (
+        <Stack>
+            <Stack.Screen name="signin" options={{headerShown: false}}/>
+            <Stack.Screen name="signup" options={{headerShown: false}}/>
+        </Stack>
+    )
+}
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -26,6 +26,7 @@ export default function RootLayout() {
   return (
     <Stack>
       <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="(auth)" options={{ headerShown: false }} />
     </Stack>
   );
 }
